181ca5452ce71c5cdd0a3cc865b4c528c87c494e
mikdrums <vasilyevmichael78@gmail.com>
2021-09-13 16:20:12 +0300

    v.0.0.1

--- src/feature-slices/counter/counter-slice.ts
+++ src/feature-slices/counter/counter-slice.ts
@@ -1,48 +1,51 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { toNormalDegree } from "../../utils/util-fuctions";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {toNormalDegree} from "../../utils/util-fuctions";
 
 
 interface CounterState {
-  _try: "win" | "lose" | null;
-  counts: ("win" | "lose" | null)[];
-  status: 'You win!'|'You lose!'|''
+    _try: "win" | "lose" | null;
+    counts: ("win" | "lose" | null)[];
+    status: 'You win!' | 'You lose!' | ''
 }
 
 
 const initialState: CounterState = {
-  _try: null,
-  counts: [],
-  status:''
+    _try: null,
+    counts: [],
+    status: ''
 };
 
 export const counterSlice = createSlice({
-  name: "counter-tries",
-  initialState,
-  reducers: {
-    incrementTries: (state) => {
-      if (state.counts.length === 5) {
+    name: "counter-tries",
+    initialState,
+    reducers: {
 
-        state.status = state.counts.filter(c => c==='win').length>2?
-            "You win!": "You lose!"
-        state.counts = []
-      }
-      state.counts.push(state._try);
-    },
-    compare: (
-      state,
-      action: PayloadAction<{ temp: number; tempServer: number }>
-    ) => {
-      const diff = Math.abs(
-        action.payload.temp - toNormalDegree(action.payload.tempServer)
-      );
+        incrementTries: (state) => {
+            if (state.counts.length === 5) {
+                state.counts = []
+            }
+            if (state.counts.length === 4) {
+                state.status = state.counts.filter(c => c === 'win').length > 2 ?
+                    "You win!" : "You lose!"
 
-      if (diff < 2) {
-        state._try = "win";
-      } else state._try = "lose";
+            } else state.status = ''
+            state.counts.push(state._try);
+        },
+        compare: (
+            state,
+            action: PayloadAction<{ temp: number; tempServer: number }>
+        ) => {
+            const diff = Math.abs(
+                action.payload.temp - toNormalDegree(action.payload.tempServer)
+            );
+
+            if (diff < 2) {
+                state._try = "win";
+            } else state._try = "lose";
+        },
     },
-  },
 });
 
-export const { incrementTries, compare } = counterSlice.actions;
+export const {incrementTries, compare} = counterSlice.actions;
 
 export default counterSlice.reducer;
